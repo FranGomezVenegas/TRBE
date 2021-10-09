dd64839954bceafc805c9a290818c54c68b32918
frangomezvenegas <info.fran.gomez@gmail.com>
2021-10-09 13:21:27 +0200

    Enable the revoke certif by expiration when expired

--- src/main/java/trazit/session/ProcReqSessionAutomatisms.java
+++ src/main/java/trazit/session/ProcReqSessionAutomatisms.java
@@ -8,7 +8,9 @@ package trazit.session;
 import databases.Rdbms;
 import databases.SqlStatement.WHERECLAUSE_TYPES;
 import databases.TblsData;
+import functionaljavaa.certification.CertifGlobalVariables;
 import functionaljavaa.parameter.Parameter;
+import lbplanet.utilities.LPDate;
 import lbplanet.utilities.LPPlatform;
 
 /**
@@ -20,6 +22,7 @@ import lbplanet.utilities.LPPlatform;
 public class ProcReqSessionAutomatisms {
     
     public static void markAsExpiredTheExpiredObjects(String procInstanceName){
+//if (1==1)return;        
         //String procInstanceName=ProcedureRequestSession.getInstanceForActions(null, null, null).getProcedureInstance();        
         String procedureActionsUserRoles = Parameter.getBusinessRuleProcedureFile(procInstanceName, LPPlatform.LpPlatformBusinessRules.MARK_EXPIRED_OBJECTS.getAreaName(), LPPlatform.LpPlatformBusinessRules.MARK_EXPIRED_OBJECTS.getTagName());        
         if (procedureActionsUserRoles==null || procedureActionsUserRoles.length()==0) return;
@@ -29,21 +32,24 @@ public class ProcReqSessionAutomatisms {
             Object[] dbTableExists = Rdbms.dbTableExists(LPPlatform.buildSchemaName(procInstanceName, curEntityInfo[0]), curEntityInfo[1], TblsData.CertifUserAnalysisMethod.FLD_CERTIF_EXPIRY_DATE.getName());
             if (LPPlatform.LAB_FALSE.equalsIgnoreCase(dbTableExists[0].toString())) return;
             Object[][] expiredRecordsArr = Rdbms.getRecordFieldsByFilter(LPPlatform.buildSchemaName(procInstanceName, curEntityInfo[0]), curEntityInfo[1], 
-                    new String[]{TblsData.CertifUserAnalysisMethod.FLD_LIGHT.getName()}, //TblsData.CertifUserAnalysisMethod.FLD_CERTIF_EXPIRY_DATE.getName()+WHERECLAUSE_TYPES.LESS_THAN.getSqlClause()
-                    new Object[]{"GREEN"}, new String[]{curEntityInfo[2]}); //LPDate.getCurrentTimeStamp()
+                    new String[]{TblsData.CertifUserAnalysisMethod.FLD_LIGHT.getName(), TblsData.CertifUserAnalysisMethod.FLD_CERTIF_EXPIRY_DATE.getName()+WHERECLAUSE_TYPES.LESS_THAN.getSqlClause()}, 
+                    new Object[]{CertifGlobalVariables.CertifLight.GREEN.toString(), LPDate.getCurrentTimeStamp()}, new String[]{curEntityInfo[2]}); 
+//            if (curEntityInfo[1].toString().equalsIgnoreCase(TblsData.CertifUserAnalysisMethod.TBL.getName())) 
+//                return expiredRecordsArr.length;
             if (!LPPlatform.LAB_FALSE.equalsIgnoreCase(expiredRecordsArr[0][0].toString())){
                 String whereFldValue="";
                 for (Object[] curObj: expiredRecordsArr){
                     if (whereFldValue.length()>0)whereFldValue=whereFldValue+"|";
                     whereFldValue=whereFldValue+curEntityInfo[3].toString()+"*"+curObj[0].toString();
                 }
-                    String[] updFldName=new String[]{TblsData.CertifUserAnalysisMethod.FLD_LIGHT.getName(), TblsData.CertifUserAnalysisMethod.FLD_STATUS.getName()};
-                    Object[] updFldValue=new Object[]{"RED", "NOT_PASS"};
-                    Rdbms.updateRecordFieldsByFilter(LPPlatform.buildSchemaName(procInstanceName, curEntityInfo[0]), curEntityInfo[1], 
-                        updFldName, updFldValue, new String[]{curEntityInfo[2].toString()+" "+WHERECLAUSE_TYPES.IN.getSqlClause()},
-                        new Object[]{whereFldValue});               
+                String[] updFldName=new String[]{TblsData.CertifUserAnalysisMethod.FLD_LIGHT.getName(), TblsData.CertifUserAnalysisMethod.FLD_STATUS.getName()};
+                Object[] updFldValue=new Object[]{CertifGlobalVariables.CertifLight.GREEN.toString(), "NOT_PASS"};
+                Rdbms.updateRecordFieldsByFilter(LPPlatform.buildSchemaName(procInstanceName, curEntityInfo[0]), curEntityInfo[1], 
+                    updFldName, updFldValue, new String[]{curEntityInfo[2].toString()+" "+WHERECLAUSE_TYPES.IN.getSqlClause()},
+                    new Object[]{whereFldValue});               
             }
         }
+        return;
     }
     
 }
