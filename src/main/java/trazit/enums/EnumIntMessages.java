69179ac50fb2ce471057dd098d017e641391a20a
frangomezvenegas <info.fran.gomez@gmail.com>
2021-12-18 15:30:39 +0100

    messageCodes enumRefactoring

---
+++ src/main/java/trazit/enums/EnumIntMessages.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package trazit.enums;
+
+/**
+ *
+ * @author User
+ */
+public interface EnumIntMessages {
+    String getErrorCode();
+    String getDefaultTextEn();
+    String getDefaultTextEs();
+/*    String getTagName();   
+    String getAreaName();
+    JSONArray getValuesList();
+    Boolean getAllowMultiValue();
+    char getMultiValueSeparator();
+    Boolean getIsOptional();    
+    ArrayList<String[]> getPreReqs();    */
+}
+
+
---
+++ src/main/java/trazit/enums/EnumIntTableFields.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package trazit.enums;
+
+/**
+ *
+ * @author User
+ */
+public interface EnumIntTableFields {
+    String getFieldName();   
+    String getFieldType();   
+    String getFieldMask();  
+    ReferenceFld getReferenceTable();     
+
+}
+
