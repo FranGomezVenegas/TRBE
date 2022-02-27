99eb0b3023330ebeabe71cefb893da7f4835a3f6
frangomezvenegas <info.fran.gomez@gmail.com>
2022-02-27 23:46:25 +0100

    Fixes for createTable script once new TableDefinition applied

--- src/main/java/trazit/enums/deployrepository/DeployTables.java
+++ src/main/java/trazit/enums/deployrepository/DeployTables.java
@@ -9,9 +9,12 @@ import databases.DbObjects;
 import functionaljavaa.businessrules.BusinessRules;
 import lbplanet.utilities.LPArray;
 import lbplanet.utilities.LPDatabase;
+import lbplanet.utilities.LPNulls;
+import lbplanet.utilities.LPPlatform;
 import trazit.enums.EnumIntTableFields;
 import trazit.enums.EnumIntTables;
 import trazit.enums.FldBusinessRules;
+import trazit.enums.ForeignkeyFld;
 
 /**
  *
@@ -29,28 +32,31 @@ public static String createTableScript(EnumIntTables tableObj){
 public static String createTableScript(EnumIntTables tableObj, String procInstanceName){
     String seqScript="";
 
-    seqScript=seqScript+sequenceScript(tableObj);
+    seqScript=seqScript+sequenceScript(tableObj, procInstanceName);
     seqScript=seqScript+createTableBeginScript(tableObj, procInstanceName);
     seqScript=seqScript+primaryKeyScript(tableObj);
     seqScript=seqScript+foreignKeyScript(tableObj);
     seqScript=seqScript+createTableEndScript();
         
-    seqScript=seqScript+alterTableScript(tableObj);
-    seqScript=seqScript+tableCommentScript(tableObj);
-    seqScript=seqScript+fieldCommentScript(tableObj);
+    seqScript=seqScript+alterTableScript(tableObj, procInstanceName);
+    seqScript=seqScript+tableCommentScript(tableObj, procInstanceName);
+    seqScript=seqScript+fieldCommentScript(tableObj, procInstanceName);
     
     
     return seqScript;
 }
 
-private static String sequenceScript(EnumIntTables tableObj){
+private static String sequenceScript(EnumIntTables tableObj, String procInstanceName){
     String SEQ_SCRIPT_POSTGRESQL="CREATE SEQUENCE #SCHEMA.#TBL_#FLD_seq INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 9223372036854775807 CACHE 1; ";
     String SEQ_SET_OWNER_SCRIPT="ALTER SEQUENCE #SCHEMA.#TBL_#FLD_seq OWNER TO #OWNER; ";
     String seqScript="";
     if (tableObj.getSeqName()!=null){
         seqScript = SEQ_SCRIPT_POSTGRESQL;
         seqScript=seqScript+SEQ_SET_OWNER_SCRIPT;
-        seqScript=seqScript.replace("#SCHEMA", tableObj.getRepositoryName()).replace("#OWNER", DbObjects.POSTGRES_DB_OWNER)
+        String schemaName=tableObj.getRepositoryName();
+        schemaName=LPPlatform.buildSchemaName(LPNulls.replaceNull(procInstanceName), schemaName);    
+        
+        seqScript=seqScript.replace("#SCHEMA", schemaName).replace("#OWNER", DbObjects.POSTGRES_DB_OWNER)
             .replace("#TBL", tableObj.getTableName()).replace("#FLD", tableObj.getSeqName());        
     }
     return seqScript;
@@ -61,14 +67,23 @@ private static String foreignKeyScript(EnumIntTables tableObj){
         seqScript=", CONSTRAINT #TBL_fkey FOREIGN KEY ";
         seqScript=seqScript.replace("#TBL", tableObj.getTableName());
         String fldsInv="";
-        seqScript=seqScript+" ("+tableObj.getForeignKey().getForeignKeyFld()+") ";
-        seqScript=seqScript+" REFERENCES "+ tableObj.getForeignKey().getReferencedSchema()+"."+tableObj.getForeignKey().getReferencedTable()
-                + "("+tableObj.getForeignKey().getReferencedField()+") MATCH SIMPLE ";
+        String frKeys="";
+        String refKeys="";
+        String refTable="";
+        for (Object curForKeyObj:  tableObj.getForeignKey()){
+            ForeignkeyFld curForKey=(ForeignkeyFld) curForKeyObj;
+            if (frKeys.length()==0)frKeys=frKeys+"(";
+            if (refKeys.length()==0)refKeys=refKeys+"(";
+            frKeys=frKeys+curForKey.getForeignKeyFld()+", ";
+            refKeys=refKeys+curForKey.getReferencedField()+", ";
+            refTable=" REFERENCES "+ curForKey.getReferencedSchema()+"."+curForKey.getReferencedTable();
+        }
+        frKeys=frKeys.substring(0, frKeys.length()-2)+")";
+        refKeys=refKeys.substring(0, refKeys.length()-2)+")";
+        seqScript=seqScript+frKeys+refTable+" "+refKeys+" MATCH SIMPLE ";
         seqScript=seqScript+"   ON UPDATE CASCADE ";
         seqScript=seqScript+"   ON DELETE CASCADE ";
-        seqScript=seqScript+"   NOT VALID ";
-
-        
+        seqScript=seqScript+"   NOT VALID ";                
     }
     return seqScript;
 }    
@@ -87,29 +102,35 @@ private static String primaryKeyScript(EnumIntTables tableObj){
     }    
     return seqScript;
 }
-private static String alterTableScript(EnumIntTables tableObj){
-    String script="";    
+private static String alterTableScript(EnumIntTables tableObj, String procInstanceName){
+    String script=""; 
+    String schemaName=tableObj.getRepositoryName();
+    schemaName=LPPlatform.buildSchemaName(LPNulls.replaceNull(procInstanceName), schemaName);    
     script=LPDatabase.POSTGRESQL_OIDS+LPDatabase.createTableSpace()+"  ALTER TABLE  #SCHEMA.#TBL" + LPDatabase.POSTGRESQL_TABLE_OWNERSHIP+";";
-    script=script.replace("#SCHEMA", tableObj.getRepositoryName()).replace("#TBL", tableObj.getTableName())
+    script=script.replace("#SCHEMA", schemaName).replace("#TBL", tableObj.getTableName())
         .replace("#OWNER", DbObjects.POSTGRES_DB_OWNER).replace("#TABLESPACE", DbObjects.POSTGRES_DB_TABLESPACE);        
     return script;
 }
 
-private static String tableCommentScript(EnumIntTables tableObj){
+private static String tableCommentScript(EnumIntTables tableObj, String procInstanceName){
     String script="";    
+    String schemaName=tableObj.getRepositoryName();
+    schemaName=LPPlatform.buildSchemaName(LPNulls.replaceNull(procInstanceName), schemaName);    
     if (tableObj.getTableComment()==null || tableObj.getTableComment().length()==0) return script;
     script="  COMMENT ON TABLE #SCHEMA.#TBL IS '" +tableObj.getTableComment()+"';";
-    script=script.replace("#SCHEMA", tableObj.getRepositoryName()).replace("#TBL", tableObj.getTableName());        
+    script=script.replace("#SCHEMA", schemaName).replace("#TBL", tableObj.getTableName());        
     return script;
 }
 
-private static String fieldCommentScript(EnumIntTables tableObj){
+private static String fieldCommentScript(EnumIntTables tableObj, String procInstanceName){
     String script="";    
+    String schemaName=tableObj.getRepositoryName();
+    schemaName=LPPlatform.buildSchemaName(LPNulls.replaceNull(procInstanceName), schemaName);    
     if (tableObj.getTableFields()==null) return script;
     for (EnumIntTableFields curFld: tableObj.getTableFields()){
         if (curFld.getFieldComment()!=null && curFld.getFieldComment().length()>0){    
             String scriptFldComment="  COMMENT ON COLUMN #SCHEMA.#TBL.#FLD IS '" +curFld.getFieldComment()+"';";
-            scriptFldComment=scriptFldComment.replace("#SCHEMA", tableObj.getRepositoryName()).replace("#TBL", tableObj.getTableName())
+            scriptFldComment=scriptFldComment.replace("#SCHEMA", schemaName).replace("#TBL", tableObj.getTableName())
                 .replace("#FLD", curFld.getName());        
             script=script+scriptFldComment;
         }
@@ -121,6 +142,8 @@ private static String createTableBeginScript(EnumIntTables tableObj, String proc
     BusinessRules bi=new BusinessRules(procInstanceName, null);
     String script="";    
     script=LPDatabase.createTable() + " (";    
+    String schemaName=tableObj.getRepositoryName();
+    schemaName=LPPlatform.buildSchemaName(LPNulls.replaceNull(procInstanceName), schemaName);
     StringBuilder fieldsScript=new StringBuilder(0);
     for (EnumIntTableFields curFld: tableObj.getTableFields()){
         StringBuilder currFieldDefBuilder = new StringBuilder(curFld.getFieldType());
@@ -131,14 +154,14 @@ private static String createTableBeginScript(EnumIntTables tableObj, String proc
                 //fieldsScript.append(curFld.getName()).append(" ").append(currFieldDefBuilder);
                 String fldSeq="";
                 fldSeq=curFld.getName()+" bigint NOT NULL DEFAULT nextval('#SCHEMA.#TBL_#FLD_seq'::regclass)";
-                fldSeq=fldSeq.replace("#SCHEMA", tableObj.getRepositoryName()).replace("#TBL", tableObj.getTableName()).replace("#FLD", curFld.getName());
+                fldSeq=fldSeq.replace("#SCHEMA", schemaName).replace("#TBL", tableObj.getTableName()).replace("#FLD", curFld.getName());
                 fieldsScript.append(fldSeq);
             }else
                 fieldsScript.append(curFld.getName()).append(" ").append(currFieldDefBuilder);        
         }
     }
     script=script+fieldsScript;
-    script=script.replace("#SCHEMA", tableObj.getRepositoryName()).replace("#TBL", tableObj.getTableName());
+    script=script.replace("#SCHEMA", schemaName).replace("#TBL", tableObj.getTableName());
     return script;
 }
 private static String createTableEndScript(){
