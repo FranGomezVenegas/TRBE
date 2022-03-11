312cdb842ce6f03c9652ec8ea7b04557d5a50556
frangomezvenegas <info.fran.gomez@gmail.com>
2022-03-12 00:18:01 +0100

    Apply new viewDefinition

--- src/main/java/com/labplanet/servicios/app/AuthenticationAPI.java
+++ src/main/java/com/labplanet/servicios/app/AuthenticationAPI.java
@@ -203,7 +203,7 @@ public class AuthenticationAPI extends HttpServlet {
                     jsonObj.put("header_info", AppHeaderAPI(request, response));
                     jsonObj.put("procedures_list", procedureListInfo(request, response));
                     jsonObj.put("all_my_sops", SopUserAPIfrontend.AllMySops(request, response));
-                    jsonObj.put("procedures_sops", SopUserAPIfrontend.ProceduresSops(request, response));
+                     jsonObj.put("procedures_sops", SopUserAPIfrontend.ProceduresSops(request, response));
                     jsonObj.put("sop_tree_list_element", SopUserAPIfrontend.SopTreeListElements(request, response));                    
                     jsonObj.put("all_my_analysis_methods", CertifyAnalysisMethodAPIfrontend.AllMyAnalysisMethodCertif(request, response));
                     jsonObj.put("platform_business_rules", AllAppBusinessRules(request, response));
