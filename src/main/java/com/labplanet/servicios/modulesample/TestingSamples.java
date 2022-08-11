1796f6ad826cf71b5d8b0353f666bc1abdefb763
frangomezvenegas <info.fran.gomez@gmail.com>
2022-08-11 13:26:32 +0200

    RefreshAdded testing for module samples

--- src/main/java/com/labplanet/servicios/modulesample/TestingSamples.java
+++ src/main/java/com/labplanet/servicios/modulesample/TestingSamples.java
@@ -5,7 +5,11 @@
  */
 package com.labplanet.servicios.modulesample;
 
+import com.labplanet.servicios.app.GlobalAPIsParams;
 import com.labplanet.servicios.app.TestingRegressionUAT;
+import com.labplanet.servicios.proceduredefinition.ReqProcedureEnums.ProcedureDefinitionAPIActionsEndpoints;
+import functionaljavaa.investigation.ClassInvestigationController;
+import functionaljavaa.parameter.Parameter;
 import functionaljavaa.testingscripts.LPTestingOutFormat;
 import functionaljavaa.testingscripts.LPTestingParams;
 import functionaljavaa.testingscripts.LPTestingParams.TestingServletsConfig;
@@ -13,6 +17,7 @@ import functionaljavaa.testingscripts.TestingAssert;
 import functionaljavaa.testingscripts.TestingAssertSummary;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.Arrays;
 import java.util.HashMap;
@@ -28,6 +33,7 @@ import lbplanet.utilities.LPFrontEnd;
 import lbplanet.utilities.LPNulls;
 import lbplanet.utilities.LPPlatform;
 import org.json.simple.JSONArray;
+import trazit.session.ApiMessageReturn;
 import trazit.session.ProcedureRequestSession;
 
 /**
@@ -46,28 +52,35 @@ public class TestingSamples extends HttpServlet {
      * @throws IOException if an I/O error occurs
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        ProcedureRequestSession instanceForActions = ProcedureRequestSession.getInstanceForActions(null, null, null);
-
         String table1Header = TestingServletsConfig.DB_SCHEMADATA_SAMPLES.getTablesHeaders();
         Integer table1NumArgs=13;
-        
+        LocalDateTime timeStarted=LPDate.getCurrentTimeStamp();
         Object[] functionEvaluation=new Object[0];
-        JSONArray functionRelatedObjects=new JSONArray();
+        JSONArray functionRelatedObjects=new JSONArray();        
         Integer scriptId=Integer.valueOf(LPNulls.replaceNull(request.getParameter("scriptId")));
 
         response = LPTestingOutFormat.responsePreparation(response);        
         TestingAssertSummary tstAssertSummary = new TestingAssertSummary();
 
-        String testerFileName=LPTestingParams.TestingServletsConfig.DB_SCHEMADATA_ENVMONIT_SAMPLES.getTesterFileName();                         
-        LPTestingOutFormat tstOut=new LPTestingOutFormat(request, LPTestingParams.TestingServletsConfig.DB_SCHEMADATA_ENVMONIT_SAMPLES.name(), testerFileName);
+        String testerFileName=LPTestingParams.TestingServletsConfig.DB_SCHEMADATA_SAMPLES.getTesterFileName();                         
+        LPTestingOutFormat tstOut=new LPTestingOutFormat(request, LPTestingParams.TestingServletsConfig.DB_SCHEMADATA_SAMPLES.name(), testerFileName);
         HashMap<String, Object> csvHeaderTags=tstOut.getCsvHeaderTags();
         
         StringBuilder fileContentBuilder = new StringBuilder(0);        
+
         fileContentBuilder.append(tstOut.getHtmlStyleHeader());
         Object[][]  testingContent =tstOut.getTestingContent();
         testingContent=LPArray.addColumnToArray2D(testingContent, new JSONArray());
         
+        String stopPhrase=null;
+        
         try (PrintWriter out = response.getWriter()) {
+            ProcedureRequestSession instanceForActions = ProcedureRequestSession.getInstanceForActions(null, null, null);
+            String procInstanceName=instanceForActions.getProcedureInstance();
+/*            String brName="sampleReviewer_canBeAnyTestingGroupReviewer";
+            String ruleValue=Parameter.getBusinessRuleProcedureFile(procInstanceName, "procedure", brName);
+            out.println("ruleValue using Parameter.getBusinessRuleProcedureFile = "+ruleValue);
+            if (1==1) return;*/
             if (csvHeaderTags.containsKey(LPPlatform.LAB_FALSE)){
                 fileContentBuilder.append("There are missing tags in the file header: ").append(csvHeaderTags.get(LPPlatform.LAB_FALSE));
                 out.println(fileContentBuilder.toString()); 
@@ -81,22 +94,68 @@ public class TestingSamples extends HttpServlet {
             fileContentTable1Builder.append(LPTestingOutFormat.createTableWithHeader(table1Header, numEvaluationArguments));
             for ( Integer iLines =numHeaderLines;iLines<testingContent.length;iLines++){
                 LocalDateTime timeStartedStep=LPDate.getCurrentTimeStamp();
+                LPTestingParams.handleAlternativeToken(tstOut, iLines);
+                
                 tstAssertSummary.increaseTotalTests();                    
                 TestingAssert tstAssert = new TestingAssert(testingContent[iLines], numEvaluationArguments);                
-                
+
                 Object actionName = LPNulls.replaceNull(testingContent[iLines][5]).toString();
+/*out.println(iLines+" "+actionName);      
+if (iLines==25){
+    out.println("stop here");
+}*/
+                request.setAttribute(GlobalAPIsParams.REQUEST_PARAM_ACTION_NAME, actionName);
+                instanceForActions.setActionNameForTesting(scriptId, iLines, actionName.toString());                
+                if (tstOut.getAuditReasonPosic()!=-1)
+                    request.setAttribute(GlobalAPIsParams.REQUEST_PARAM_AUDIT_REASON_PHRASE, LPNulls.replaceNull(testingContent[iLines][tstOut.getAuditReasonPosic()]).toString());
+
                 fileContentTable1Builder.append(LPTestingOutFormat.rowAddFields(
                     new Object[]{iLines-numHeaderLines+1, "actionName"+":"+LPNulls.replaceNull(testingContent[iLines][5]).toString()}));                     
-                instanceForActions.setActionNameForTesting(scriptId, iLines, actionName.toString());
 
-                ClassSampleController clssController=new ClassSampleController(request, actionName.toString(), testingContent, iLines, table1NumArgs);
-                if (clssController.getFunctionFound()){
-                    functionRelatedObjects=clssController.getFunctionRelatedObjects();
-                    functionEvaluation=(Object[]) clssController.getFunctionDiagn();
+                if (actionName.toString().equalsIgnoreCase(ProcedureDefinitionAPIActionsEndpoints.SET_PROCEDURE_BUSINESS_RULES.getName())){
+                    procInstanceName=LPNulls.replaceNull(testingContent[iLines][6]).toString();
+                    fileContentTable1Builder.append(procInstanceName);                    
+                    String suffixName=LPNulls.replaceNull(testingContent[iLines][7]).toString();
+                    fileContentTable1Builder.append(suffixName);                    
+                    String propName=LPNulls.replaceNull(testingContent[iLines][8]).toString();
+                    fileContentTable1Builder.append(propName);                    
+                    String propValue=LPNulls.replaceNull(testingContent[iLines][9]).toString();
+                    fileContentTable1Builder.append(propValue);                    
+                    Parameter parm=new Parameter();
+//                    parm.createPropertiesFile(Parameter.PropertyFilesType.PROCEDURE_BUSINESS_RULES_DIR_PATH.name(),  
+//                    procInstanceName+"-"+suffixName);  
+                    String diagn=parm.addTagInPropertiesFile(Parameter.PropertyFilesType.PROCEDURE_BUSINESS_RULES_DIR_PATH.name(),  
+                        procInstanceName+"-"+suffixName, propName, propValue);
+                    functionRelatedObjects=new JSONArray();                      
+                    if (diagn.toUpperCase().contains("CREATED"))
+                        functionEvaluation=ApiMessageReturn.trapMessage(LPPlatform.LAB_TRUE, LPPlatform.LpPlatformSuccess.PROPERTY_CREATED, new Object[]{diagn});
+                    else
+                        functionEvaluation=ApiMessageReturn.trapMessage(LPPlatform.LAB_FALSE, LPPlatform.ApiErrorTraping.PROPERTY_NOT_CREATED, new Object[]{diagn});
                     testingContent[iLines][testingContent[0].length-1]=functionRelatedObjects;
-                    fileContentTable1Builder.append(clssController.getRowArgsRows());
-                }                
-                
+                    
+                }else{    
+                    ClassSampleController clssSampleController=new ClassSampleController(request, actionName.toString(), testingContent, iLines, table1NumArgs);
+                    if (clssSampleController.getFunctionFound()){
+                        functionRelatedObjects=clssSampleController.getFunctionRelatedObjects();
+                        functionEvaluation=(Object[]) clssSampleController.getFunctionDiagn();
+                        testingContent[iLines][testingContent[0].length-1]=functionRelatedObjects;
+                        fileContentTable1Builder.append(clssSampleController.getRowArgsRows());                
+                    }else{
+                        ClassInvestigationController clssInvestigationController=new ClassInvestigationController(request, actionName.toString(), testingContent, iLines, table1NumArgs);
+                        if (clssInvestigationController.getFunctionFound()){
+                            functionRelatedObjects=clssInvestigationController.getFunctionRelatedObjects();
+                            functionEvaluation=(Object[]) clssInvestigationController.getFunctionDiagn();
+                            testingContent[iLines][testingContent[0].length-1]=functionRelatedObjects;
+                            fileContentTable1Builder.append(clssInvestigationController.getRowArgsRows());                
+                        }else{
+                            functionEvaluation=ApiMessageReturn.trapMessage(LPPlatform.LAB_FALSE, LPPlatform.ApiErrorTraping.PROPERTY_ENDPOINT_NOT_FOUND, new Object[]{actionName});
+                            testingContent[iLines][testingContent[0].length-1]=functionRelatedObjects;
+                            fileContentTable1Builder.append(clssSampleController.getRowArgsRows());         
+                        }
+                        clssInvestigationController=null;
+                    }
+                    clssSampleController=null;
+                }
                 if (testingContent[iLines][0]==null){tstAssertSummary.increasetotalLabPlanetBooleanUndefined();}
                 if (testingContent[iLines][1]==null){tstAssertSummary.increasetotalLabPlanetErrorCodeUndefined();}
                                     
@@ -105,40 +164,62 @@ public class TestingSamples extends HttpServlet {
                     (LPNulls.replaceNull(testingContent[iLines][2]).toString().length()>0) ? "Yes" : "No",
                     (LPNulls.replaceNull(testingContent[iLines][3]).toString().length()>0) ? "Yes" : "No",
                     (LPNulls.replaceNull(testingContent[iLines][4]).toString().length()>0) ? "Yes" : "No",
-                }));                                     
-
+                }));  
+                BigDecimal SecondsInDateRange = LPDate.SecondsInDateRange(timeStartedStep, LPDate.getCurrentTimeStamp(), true);
+                fileContentTable1Builder.append(LPTestingOutFormat.rowAddField(String.valueOf(SecondsInDateRange)));
                 if (numEvaluationArguments==0){                    
                     fileContentTable1Builder.append(LPTestingOutFormat.rowAddField(Arrays.toString(functionEvaluation)));                     
                 }                                
                 if (numEvaluationArguments>0){                    
                     Object[] evaluate = tstAssert.evaluate(numEvaluationArguments, tstAssertSummary, functionEvaluation);   
+                        
                     Integer stepId=Integer.valueOf(testingContent[iLines][tstOut.getStepIdPosic()].toString());
-                    fileContentTable1Builder.append(tstOut.publishEvalStep(request, stepId, functionEvaluation, functionRelatedObjects, tstAssert, timeStartedStep));
+                    fileContentTable1Builder.append(tstOut.publishEvalStep(request, stepId, functionEvaluation, functionRelatedObjects, tstAssert, timeStartedStep));                    
                     fileContentTable1Builder.append(LPTestingOutFormat.rowAddFields(evaluate));                        
+                    if ( tstOut.getStopSyntaxisUnmatchPosic()>-1 && Boolean.valueOf(LPNulls.replaceNull(testingContent[iLines][tstOut.getStopSyntaxisUnmatchPosic()]).toString()) &&
+                            !TestingAssert.EvalCodes.MATCH.toString().equalsIgnoreCase(tstAssert.getEvalSyntaxisDiagnostic()) ){
+                        out.println(fileContentBuilder.toString()); 
+                        stopPhrase="Interrupted by evaluation not matching in step "+(iLines+1)+" of "+testingContent.length;
+//                        Rdbms.updateRecordFieldsByFilter(LPPlatform.buildSchemaName(procInstanceName.toString(), GlobalVariables.Schemas.TESTING.getName()), TblsTesting.TablesTesting.SCRIPT.getTableName(), 
+//                                new String[]{TblsTesting.Script.RUN_SUMMARY.getName()}, new Object[]{"Interrupted by evaluation not matching in step "+(iLines+1)+" of "+testingContent.length}, 
+//                                new String[]{TblsTesting.Script.SCRIPT_ID.getName()}, new Object[]{6}); //testingContent[iLines][tstOut.getScriptIdPosic()]});
+                        break;      
+                    }
                 }
-                fileContentTable1Builder.append(LPTestingOutFormat.rowEnd());                                                
+                if (tstOut.getStopSyntaxisFalsePosic()>-1 && Boolean.valueOf(LPNulls.replaceNull(testingContent[iLines][tstOut.getStopSyntaxisFalsePosic()]).toString())
+                    && LPPlatform.LAB_FALSE.equalsIgnoreCase(functionEvaluation[0].toString())){
+                        out.println(fileContentBuilder.toString()); 
+                        stopPhrase="Interrupted by evaluation returning false in step "+(iLines+1)+" of "+testingContent.length;
+//                        Rdbms.updateRecordFieldsByFilter(LPPlatform.buildSchemaName(procInstanceName.toString(), GlobalVariables.Schemas.TESTING.getName()), TblsTesting.TablesTesting.SCRIPT.getTableName(), 
+//                                new String[]{TblsTesting.Script.RUN_SUMMARY.getName()}, new Object[]{"Interrupted by evaluation returning false "+(iLines+1)+" of "+testingContent.length}, 
+//                                new String[]{TblsTesting.Script.SCRIPT_ID.getName()}, new Object[]{6}); //testingContent[iLines][tstOut.getScriptIdPosic()]});
+                    break;
+                }
+                fileContentTable1Builder.append(LPTestingOutFormat.rowEnd());   
+                instanceForActions.auditActionsKill();
             }    
             fileContentTable1Builder.append(LPTestingOutFormat.tableEnd());
-            fileContentTable1Builder.append(LPTestingOutFormat.businessRulesTable());            
-            fileContentBuilder.append(tstOut.publishEvalSummary(request, tstAssertSummary));
+            fileContentTable1Builder.append(LPTestingOutFormat.businessRulesTable());
+            fileContentBuilder.append(tstOut.publishEvalSummary(request, tstAssertSummary, stopPhrase, timeStarted)).append("<br>");
             fileContentBuilder.append(fileContentTable1Builder).append(LPTestingOutFormat.bodyEnd()).append(LPTestingOutFormat.htmlEnd());
 
             out.println(fileContentBuilder.toString());            
-            LPTestingOutFormat.createLogFile(tstOut.getFilePathName(), fileContentBuilder.toString());
+            //LPTestingOutFormat.createLogFile(tstOut.getFilePathName(), fileContentBuilder.toString());
             tstAssertSummary=null; 
         }
         catch(IOException error){
-            //tstAssertSummary=null; 
+            tstAssertSummary=null; 
             String exceptionMessage = error.getMessage();     
             LPFrontEnd.servletReturnResponseError(request, response, exceptionMessage, null, null, null);                    
-        } finally {
+        } finally {    
             // release database resources
             try {
+                ProcedureRequestSession.getInstanceForActions(request, response, Boolean.TRUE).killIt();
                 // Rdbms.closeRdbms();   
             } catch (Exception ex) {Logger.getLogger(this.getClass().getName()).log(Level.SEVERE, null, ex);
             }
         }               
-    }
+    }   
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /**
