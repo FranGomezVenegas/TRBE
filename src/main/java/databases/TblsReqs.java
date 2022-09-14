799a06905678527040fc7849ce92006c144726ba
frangomezvenegas <info.fran.gomez@gmail.com>
2022-09-14 02:14:37 +0200

    Branch_level no longer need

--- src/main/java/databases/TblsReqs.java
+++ src/main/java/databases/TblsReqs.java
@@ -282,17 +282,19 @@ public class TblsReqs {
 //        SCHEMA_PREFIX(FIELDS_NAMES_SCHEMA_PREFIX, LPDatabase.stringNotNull(), null, null, null, null),
         ORDER_NUMBER("order_number", LPDatabase.integer(), null, null, null, null),
         NAME("name", LPDatabase.string(), null, null, null, null),
+        LP_FRONTEND_PAGE_NAME("lp_frontend_page_name", LPDatabase.string(), null, null, null, null),
+        //PARENT_NAME("parent_name", LPDatabase.string(), null, null, null, null),
+        POSITION("position", LPDatabase.integer(), null, null, null, null),
         ROLE_NAME("role_name", LPDatabase.string(), null, null, null, null),
         MODE("mode", LPDatabase.string(), null, null, null, null),
         TYPE("type", LPDatabase.string(), null, null, null, null),
-        BRANCH_LEVEL("branch_level", LPDatabase.string(), null, null, null, null),
         LABEL_EN("label_en", LPDatabase.string(), null, null, null, null),
         LABEL_ES("label_es", LPDatabase.string(), null, null, null, null),
         SOP("sop", LPDatabase.string(), null, null, null, null),
         ESIGN_REQUIRED("esign_required", LPDatabase.booleanFld(), null, null, null, null),
         USERCONFIRM_REQUIRED("userconfirm_required", LPDatabase.booleanFld(), null, null, null, null),
-        LP_FRONTEND_PAGE_NAME("lp_frontend_page_name", LPDatabase.string(), null, null, null, null),
-        // ....
+        ICON_NAME("icon_name", LPDatabase.string(), null, null, null, null),
+        ICON_NAME_WHEN_NOT_CERTIFIED("icon_name_when_not_certified", LPDatabase.string(), null, null, null, null)
         ;
         private ProcedureUserRequirementsEvents(String dbObjName, String dbObjType, String fieldMask, ReferenceFld refer, String comment,
                 FldBusinessRules[] fldBusRules){
