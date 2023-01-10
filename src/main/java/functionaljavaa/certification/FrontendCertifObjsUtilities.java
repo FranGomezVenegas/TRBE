ad6956fbb4e0d77f1513228a836a38df02b7dd66
frangomezvenegas <info.fran.gomez@gmail.com>
2023-01-10 01:07:05 +0100

    new certification mode, understood and sent for reviewer2

--- src/main/java/functionaljavaa/certification/FrontendCertifObjsUtilities.java
+++ src/main/java/functionaljavaa/certification/FrontendCertifObjsUtilities.java
@@ -19,7 +19,9 @@ public class FrontendCertifObjsUtilities {
     
     public enum CertifModes{
         CERTIFUSER_READ_AND_UNDERSTOOD("CERTIFUSER_READ_AND_UNDERSTOOD", "Read and understood", "Leido y entendido"),
-        CERTIFUSER_UNDERSTOOD_AND_SENDTOREVIEWER("CERTIFUSER_UNDERSTOOD_AND_SENDTOREVIEWER", "Understood", "Entendido"),
+        CERTIFUSER_REVIEWER_ONLY("CERTIFUSER_REVIEWER_ONLY", "Pending Approval", "Pendiente Aprobación"),         
+        CERTIFUSER_UNDERSTOOD_AND_SENDTOREVIEWER("CERTIFUSER_UNDERSTOOD_AND_SENDTOREVIEWER", "Understood and sending to reviewer", "Entendido y Envío a Aprobar"),
+        CERTIFUSER_REVIEWER_SIGN("CERTIFUSER_REVIEWER_SIGN", "Pending Approval", "Pendiente Aprobación"),        
         CERTIFUSER_TRAINING_REQUIRED("CERTIFUSER_TRAINING_REQUIRED", "Training required", "Requiere entrenamiento"),
         ;
         private CertifModes(String endpointName, String labelEn, String labelEs){
@@ -52,6 +54,36 @@ public class FrontendCertifObjsUtilities {
             return labelEs;
         }
     }    
+    public static JSONObject certifObjCertifReviewerPendingSign(String[] objFldNames, Object[] objFldValues){
+        JSONObject mainObj=new JSONObject();
+        Integer valuePosicInArray = LPArray.valuePosicInArray(objFldNames, TblsData.ViewUserAndAnalysisMethodCertificationView.LIGHT.getName());
+        if ( valuePosicInArray>1 && "GREEN".equalsIgnoreCase(LPNulls.replaceNull(objFldValues[valuePosicInArray].toString())) ){
+            mainObj.put("action_visible", false);
+            return mainObj;
+        }
+        valuePosicInArray = LPArray.valuePosicInArray(objFldNames, TblsCnfg.SopMetaData.CERTIFICATION_MODE.getName());
+        if (valuePosicInArray==-1){
+            mainObj.put("action_visible", false);
+            mainObj.put("error", TblsCnfg.SopMetaData.CERTIFICATION_MODE.getName()+" field not found to determine the mode, please review definition");
+            return mainObj;
+        }
+        mainObj.put("user_name", objFldValues[LPArray.valuePosicInArray(objFldNames, TblsData.ViewUserAndAnalysisMethodCertificationView.USER_NAME.getName())]);
+        valuePosicInArray = LPArray.valuePosicInArray(objFldNames, TblsData.ViewUserAndMetaDataSopView.PENDING_REVIEW.getName());
+        if (valuePosicInArray==-1||!Boolean.TRUE.toString().equalsIgnoreCase(LPNulls.replaceNull(objFldValues[valuePosicInArray]).toString())){
+            mainObj.put("action_visible", true);
+            mainObj.put("action_enabled", false);    
+            
+            mainObj.put("label_en", "Still not pending for approval");
+            mainObj.put("label_es", "Aún no pendiente de aprobar");
+        }else{
+            mainObj.put("action_visible", true);
+            mainObj.put("action_enabled", true);       
+            mainObj.put("endpoint_name", CertifModes.CERTIFUSER_REVIEWER_SIGN.getEndpointName());
+            mainObj.put("label_en", CertifModes.CERTIFUSER_REVIEWER_SIGN.getLabelEn());
+            mainObj.put("label_es", CertifModes.CERTIFUSER_REVIEWER_SIGN.getLabelEs());
+        }
+        return mainObj;
+    }
     
     public static JSONObject certifObjCertifModeOwnUserAction(String[] objFldNames, Object[] objFldValues){
         JSONObject mainObj=new JSONObject();
@@ -66,16 +98,28 @@ public class FrontendCertifObjsUtilities {
             mainObj.put("error", TblsCnfg.SopMetaData.CERTIFICATION_MODE.getName()+" field not found to determine the mode, please review definition");
             return mainObj;
         }
-        String certifMode =objFldValues[valuePosicInArray].toString();
+        String certifMode =LPNulls.replaceNull(objFldValues[valuePosicInArray]).toString();
         CertifModes cMode=null;
         try{
-            cMode = CertifModes.valueOf(certifMode.toUpperCase());
-            if ( (CertifModes.CERTIFUSER_READ_AND_UNDERSTOOD.toString().equalsIgnoreCase(certifMode)) || (CertifModes.CERTIFUSER_UNDERSTOOD_AND_SENDTOREVIEWER.toString().equalsIgnoreCase(certifMode))){
-                mainObj.put("action_visible", true);
-                mainObj.put("action_enabled", true);
-            }else{
-                mainObj.put("action_visible", true);
+            valuePosicInArray = LPArray.valuePosicInArray(objFldNames, TblsData.ViewUserAndMetaDataSopView.PENDING_REVIEW.getName());
+            if (valuePosicInArray>-1&&Boolean.TRUE.toString().equalsIgnoreCase(LPNulls.replaceNull(objFldValues[valuePosicInArray]).toString())){
+                mainObj.put("action_visible", false);
                 mainObj.put("action_enabled", false);
+                mainObj.put("label_en", CertifModes.CERTIFUSER_REVIEWER_SIGN.getLabelEn());
+                mainObj.put("label_es", CertifModes.CERTIFUSER_REVIEWER_SIGN.getLabelEs());                
+            }else{
+                cMode = CertifModes.valueOf(certifMode.toUpperCase());
+                if ( (CertifModes.CERTIFUSER_READ_AND_UNDERSTOOD.toString().equalsIgnoreCase(certifMode)) || (CertifModes.CERTIFUSER_UNDERSTOOD_AND_SENDTOREVIEWER.toString().equalsIgnoreCase(certifMode))){
+                    mainObj.put("action_visible", true);
+                    mainObj.put("action_enabled", true);
+                }else if (CertifModes.CERTIFUSER_UNDERSTOOD_AND_SENDTOREVIEWER.toString().equalsIgnoreCase(certifMode)){
+                    mainObj.put("action_visible", true);
+
+                    mainObj.put("action_enabled", true);
+                }else{
+                    mainObj.put("action_visible", true);
+                    mainObj.put("action_enabled", false);
+                }
             }
             mainObj.put("endpoint_name", certifMode);
             mainObj.put("label_en", cMode.getLabelEn());
