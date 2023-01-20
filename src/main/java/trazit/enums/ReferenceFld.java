8d6628cc0c382121108ac510b80c4d479845fb7e
frangomezvenegas <info.fran.gomez@gmail.com>
2023-01-21 00:27:05 +0100

    Enhancement, references to be not just a foreign key, allow a list of values

--- src/main/java/trazit/enums/ReferenceFld.java
+++ src/main/java/trazit/enums/ReferenceFld.java
@@ -11,17 +11,28 @@ package trazit.enums;
  */
 public class ReferenceFld {
         
+    private final String type;   
     private String repository;
     private String tableName;
     private String fieldName;
+    private String values;
     
     public ReferenceFld(String repo, String tbl, String fld){
+        this.type="table";
         this.repository=repo;
         this.tableName=tbl;
         this.fieldName=fld;
     }
+    public ReferenceFld(String values){
+        this.type="values";
+        this.values=values;
+        //CHECK (element_type IN ('lesson', 'quiz')
+    }
     public String getRepository(){return repository;}
     public String getTableName(){return tableName;}
     public String getFieldName(){return fieldName;}
+    public String getType(){return type;}
+    public String getValues(){return values;}
+    
     
 }
