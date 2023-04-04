3a0a20481021e3ae62d6414dd0ff6d0c8bfad5fd
frangomezvenegas <info.fran.gomez@gmail.com>
2023-04-04 11:02:26 +0200

    new notif entry when no culture media available

--- src/main/java/functionaljavaa/moduleenvironmentalmonitoring/EnvMonEnums.java
+++ src/main/java/functionaljavaa/moduleenvironmentalmonitoring/EnvMonEnums.java
@@ -16,8 +16,8 @@ public class EnvMonEnums {
         LOGSAMPLE_PROGRAM_OR_LOCATION_NOTFOUND("EnvMonSampleProgramOrLocationNotFound", "Program <*1*> or location <*2*> not found", ""),
         PERSONAL_ANALYSIS_REQUIRED_NOT_DEFINED("EnvMonSamplePersonalAnalysisRequiredNotDefined", "", ""),
         MICROORGANISM_FOUND("EnvMonSampleMicroorganismNotFound", "",""),
-        CULTURE_MEDIA_ALREADY_ASSIGNED("EnvMonSampleCultureMediaAlreadyAssigned", "",""),
-        
+        CULTURE_MEDIA_ALREADY_ASSIGNED("EnvMonSampleCultureMediaAlreadyAssigned", "",""),  
+        NOT_AVAILABLEFORUSE_CULTURE_MEDIA_LOT("EnvMonNotAvailableForUseCultureMediaLot", "","")
         ;
         private EnvMonitErrorTrapping(String errCode, String defaultTextEn, String defaultTextEs){
             this.errorCode=errCode;
