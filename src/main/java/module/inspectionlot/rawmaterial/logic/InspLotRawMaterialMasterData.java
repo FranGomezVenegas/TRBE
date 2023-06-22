482d6600fe0bf5ee8ca4e792d560b7b1496a077b
frangomezvenegas <info.fran.gomez@gmail.com>
2023-06-22 18:03:29 +0200

    Added logic for sharing master data for this given module

---
+++ src/main/java/module/inspectionlot/rawmaterial/logic/InspLotRawMaterialMasterData.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package module.inspectionlot.rawmaterial.logic;
+
+import com.labplanet.servicios.modulesample.ClassSampleQueries;
+import databases.TblsCnfg;
+import org.json.simple.JSONObject;
+import trazit.enums.FrontendMasterData;
+
+/**
+ *
+ * @author User
+ */
+public class InspLotRawMaterialMasterData implements FrontendMasterData{
+
+    @Override
+    public JSONObject getMasterDataJsonObject(String alternativeProcInstanceName) {
+        JSONObject jSummaryObj=new JSONObject();        
+        jSummaryObj.put(TblsCnfg.TablesConfig.ANALYSIS_METHOD.getTableName(), ClassSampleQueries.configAnalysisList(alternativeProcInstanceName));
+        return jSummaryObj;
+    }
+    
+}
