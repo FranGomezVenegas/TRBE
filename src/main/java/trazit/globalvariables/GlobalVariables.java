803e3978795653cd009d94030b468f90c98aa05e
frangomezvenegas <info.fran.gomez@gmail.com>
2023-11-05 19:16:58 +0100

    validation mode repository typo, - instead of _

--- src/main/java/trazit/globalvariables/GlobalVariables.java
+++ src/main/java/trazit/globalvariables/GlobalVariables.java
@@ -33,7 +33,7 @@ public class GlobalVariables {
     public static final String DEFAULTLANGUAGE="en";
     public static final String TRAZIT_SCHEDULER="TRAZIT_SCHEDULER";
     public static final String PROC_MANAGEMENT_SPECIAL_ROLE="proc_management";
-    public static final String VALIDATION_MODE_REPO="valid_";
+    public static final String VALIDATION_MODE_REPO="valid-";
     
     public static final String LANGUAGE_ALL_LANGUAGES="ALL";
     public enum Languages{EN("en"), ES("es")
