4f82aad70d32764f10830e94d2db7fb2177d2a4a
frangomezvenegas <info.fran.gomez@gmail.com>
2023-12-31 00:14:14 +0100

    new endpoint

--- src/main/java/module/inspectionlot/rawmaterial/definition/InspLotRMEnums.java
+++ src/main/java/module/inspectionlot/rawmaterial/definition/InspLotRMEnums.java
@@ -367,6 +367,9 @@ public class InspLotRMEnums {
         INVESTIGATION_DETAIL_FOR_GIVEN_INVESTIGATION("INVESTIGATION_DETAIL_FOR_GIVEN_INVESTIGATION", "", new LPAPIArguments[]{new LPAPIArguments(InvestigationAPI.ParamsList.INVESTIGATION_ID.getParamName(), LPAPIArguments.ArgumentType.INTEGER.toString(), true, 6),}, EndPointsToRequirements.endpointWithNoOutputObjects,
                 null, null),
         GET_MATERIALS("GET_MATERIALS", "", new LPAPIArguments[]{}, null, null, null),    
+        GET_ANALYSIS("GET_ANALYSIS", "",
+            new LPAPIArguments[]{new LPAPIArguments("code", LPAPIArguments.ArgumentType.STRING.toString(), true, 6),
+            new LPAPIArguments("includeMethodsCertification", LPAPIArguments.ArgumentType.BOOLEAN.toString(), false, 7),}, null, null, null),
         GET_SPECS("GET_SPECS", "",
             new LPAPIArguments[]{new LPAPIArguments("specCode", LPAPIArguments.ArgumentType.STRING.toString(), true, 6)}, null, null, null),
         GET_SAMPLE_ANALYSIS_RESULT_LIST("GET_SAMPLE_ANALYSIS_RESULT_LIST", "", new LPAPIArguments[]{
