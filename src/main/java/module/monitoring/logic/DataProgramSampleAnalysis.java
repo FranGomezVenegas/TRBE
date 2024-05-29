3e9fffe54bb7a06cde3225e13bfeb6f0eea6534b
frangomezvenegas <info.fran.gomez@gmail.com>
2024-05-29 22:42:02 +0200

    calcs

--- src/main/java/module/monitoring/logic/DataProgramSampleAnalysis.java
+++ src/main/java/module/monitoring/logic/DataProgramSampleAnalysis.java
@@ -154,6 +154,11 @@ public class DataProgramSampleAnalysis implements DataSampleAnalysisStrategy {
 */
     }
 
+    @Override
+    public InternalMessage calcsPostEnterResult(Integer resultId, Integer testId, Integer sampleId, DataSample dataSample) {
+        return new InternalMessage(LPPlatform.LAB_TRUE, LPPlatform.LpPlatformSuccess.ALL_FINE, null);
+    }
+
 
   
 }
